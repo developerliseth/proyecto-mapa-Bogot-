5aa26cdb5e7414ae44c651b32f2368990cede758
developerliseth <ospina15laura@gmail.com>
2024-06-25 10:51:17 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React proyecto de mapa web bogots</h1>
+      <h1>Vite + React proyecto de mapa web BOGOTA</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
